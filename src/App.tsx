8f1bbe4268695d1cae8dd27d12c273cd4437fc4b
Matthieu Ramanitrera <mrama100@uottawa.ca>
2026-04-14 13:49:23 -0400

    feat: setup string structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Fretboard from "./components/Fretboard.tsx"
 
 function App() {
+  return (
+    <div>
+      <h1>Fretboard</h1>
+      <Fretboard />
+    </div>
 
+  )
 }
 
 export default App
